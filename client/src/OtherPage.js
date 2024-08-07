31b6e1aeb4f50b5d53591f0dcd58d33192fdcdd5
ashutiwary19 <ashu.tiwary19@gmail.com>
2024-08-07 07:29:00 +0530

    Added new project with multi container

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const otherpage = () => {
+  return (
+    <div>
+      I am some other page!
+      <Link to="/">Go Back Home</Link>
+    </div>
+  );
+};
+
+export default otherpage;
